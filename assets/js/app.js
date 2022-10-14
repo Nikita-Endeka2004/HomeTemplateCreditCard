f4e48ce905408474ccebd36229e0d6e24f4fe498
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-10-14 13:54:59 +0300

    start

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+
+	let data = "Pellentesque ex eros, +380665433321 porttitor eu bibendum ac, aliquam tincidunt urna. Mauris tristique lobortis orci, nec varius magna convallis interdum. Etiam 5363 4567 8765 3454 pharetra tempor ex, vel eleifend (067) 678 44 21 odio lacinia (0562) 35-30-38 eget. Morbi maximus libero vitae aliquet facilisis. Vivamus 5674-2346-8945-0012 vitae quam nisi. Quisque 12/45 quis venenatis 5192722517688913 lacus. Sed ac lorem (050)567-45-33 nec leo pharetra 4556796335044346 dapibus sed eu +38067432112 ex. In hac 4913-8185-2881-4543  habitasse platea dictumst. In dignissim 5461158320267908 suscipit rutrum. Ut 4916 8494 1754 2904 luctus  sapien in risus 56th street auctor, ac placerat 067-678-44-21  quam malesuada. Pellentesque (056) 7783322 bibendum justo  5363 4567 87653 3455 tempus purus convallis, a viverra nunc ullamcorper. Nulla 5213 9203 2475 5355 eget lectus gravida, porta eros vitae, semper erat +39-926-1234567.  Aenean volutpat vehicula dui ut pharetra.";
+	
+	/*
+		
+	Необходимо выбрать !!валидные!! номера банковских карт и вывести их в формате 4 блока по 4 цифры.
+	
+	Например:
+	
+	5475 0330 3843 6453
+	4539 1799 4927 2557
+	...
+
+	4556796335044346 ^[0-9]{16}$
+	
+	*/
+
+	let re = /[0-9]{16}/g;
+
+	let re2 = /([0-9]{4}).([0-9]{4}).([0-9]{4}).([0-9]{4})/g;
+
+	let result = data.match(re);
+
+	let result2 = data.match(re2);
+	
+	const rep = /\-|\ |\./g;
+
+	function checking(num){
+
+		let test = false;
+		let sum = 0;
+
+		for(let i = 0; i < num.length; i++){
+			if(i % 2 == 0){
+				sum = sum + num[i];
+			}else {
+				let n = num[i] * 2;
+				sum = sum + (n > 9 ? n - 9 : n);
+			}
+		}
+	
+		if((sum % 10) == 0){
+			test = true;
+		}else{
+			test = false;
+		}
+
+		return test;
+
+	}
+
+	// for (let item of result2) {
+
+	// 	result2.slice(0,result2.length).replace(rep, '');
+
+	// 	console.log(result2);
+		
+	// }
+
+	console.log(result2);
+	console.log(result);
+
+	for (let item of result) {
+
+		item = Number(item);
+
+		//console.log(item, typeof(item));
+
+		if(checking(item)){
+
+			console.log('Vallible', item);
+
+		}else{
+
+			console.log('No vallible', item);
+
+		}
+		
+	}
